19aebe5b5d0136261333a1eee35173fbcdb1dbc8
javierdichiachio <javierdichiachio@gmail.com>
2024-09-17 15:03:29 -0300

    app improvements

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,7 @@ def main():
                 with st.spinner(text="In progress..."):
                     answer = agent.run(user_question)
                     st.session_state.chat_history.append((user_question, answer))
-                    st.session_state['user_question'] = ''  # Clear the text input
+            st.session_state['user_question'] = ''  # Clear the text input
             st.rerun()
         
         if st.session_state['chat_history']:
