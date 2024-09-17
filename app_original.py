aa68cd8e977a9af69821422f3ca0d2064c0bb689
javierdichiachio <javierdichiachio@gmail.com>
2024-09-17 13:35:54 -0300

    App improvements

--- app_original.py
+++ app_original.py
@@ -14,29 +14,18 @@ def main():
     placeholder="Paste your openAI API key, sk-",
     type="password")
 
+
+
     csv_file = st.file_uploader("Upload a CSV file", type="csv")
-    
-    if 'chat_history' not in st.session_state:
-        st.session_state['chat_history'] = []
-    
-    if csv_file is not None and user_api_key:
-        llm = ChatOpenAI(model="gpt-4o", temperature=0, openai_api_key=user_api_key)
-        
+    if csv_file is not None:
+        llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0, openai_api_key=user_api_key)
         agent = create_csv_agent(llm,csv_file,agent_type="openai-tools",verbose=True, allow_dangerous_code=True)
 
         user_question = st.text_input("Ask a question about the LPs: ")
 
         if user_question is not None and user_question != "":
             with st.spinner(text="In progress..."):
-                answer = agent.run(user_question)
-                st.session_state.chat_history.append((user_question, answer))
-        
-        if st.session_state['chat_history']:
-            st.write("### Chat History:")
-            for question, response in st.session_state['chat_history']:
-                st.write(f"**You:** {question}")
-                st.write(f"**Bot:** {response}")
-
+                st.write(agent.run(user_question))
 
 
 if __name__ == "__main__":
