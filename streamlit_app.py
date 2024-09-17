eb55fc78e55e20ce92edbe67223b365faf1f50c4
javierdichiachio <javierdichiachio@gmail.com>
2024-09-17 14:26:43 -0300

    app improvements

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,8 +38,7 @@ def main():
                         answer = agent.run(user_question)
                         st.session_state.chat_history.append((user_question, answer))
                         st.session_state['user_question'] = ''
-                        question_container.empty()
-                        st.text_input("Ask a question about the LPs:", value='', key='user_question')
+                        st.experimental_rerun()
         
         if st.session_state['chat_history']:
             st.write("### Chat History:")
